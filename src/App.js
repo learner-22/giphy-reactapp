98214f030bb3363e2c55d6d9027638114eae4e7d
learner-22 <nbhooma@gmail.com>
2022-07-11 11:52:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
+class App extends Component {
+  
+  state={
+    giphy :[],
+    gifimg:''
+  }
+  componentDidMount(){
+    console.log('Hello from Component did mount')
+   fetch(' https://api.giphy.com/v1/gifs/trending?api_key=VMelPU46dFBVBQDlWKC9rl6XB1k3FvzJ&limit=25&rating=g')
+   .then(response =>response.json())
+ 
+   .then(json => {
+    // console.log(json)
+    this.setState({giphy :json.data}
+    )
+     } )
+
+   }
+
+
+  render(){
+   
+    return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Giphy </h1>
+      <button onClick = {this.handleClick}> Make API Call</button>
+    
+     
+       {/* {this.state.giphy.map(data => <p> <img src ={data.images.original.url}/> </p>)}  */}
+     {
+      this.state.giphy.map(g=>(
+        <div key ={g.id}>
+          <img src={g.images.original.url} alt ={g.title} />
+          <h3> {g.title}</h3>
+        </div>
+      ))
+
+     }
     </div>
   );
+  }
 }
 
 export default App;
